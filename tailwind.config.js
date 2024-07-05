11fe6b28c0fb4700195f941353adf056f0ce0532
korkiat0 <korkiat_bang@hotmail.co.th>
2024-07-05 14:46:42 +0700

    animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,9 +21,31 @@ export default {
             transform: "translate3d(0, 0, 0)",
           },
         },
+        "fade-in-left": {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(-100%, 0, 0)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+        "fade-in-up": {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(0, 100%, 0)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
       },
       animation: {
         fadeinright: "fade-in-right 1.5s ease-in-out 0.25s 1",
+        fadeinleft: "fade-in-left 1.5s ease-in-out 0.25s 1",
+        fadeinup: "fade-in-up 1s ease-in-out 0.25s 1",
       },
     },
   },
