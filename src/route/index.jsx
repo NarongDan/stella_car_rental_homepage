b7c4696de414ce8f4e7af0c1b921a6a479374b30
NarongDan <narong.dan1@gmail.com>
2024-07-05 09:32:59 +0700

    onScreen

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import { lazy } from "react";
 import { RouterProvider } from "react-router-dom";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import HomePageLayout from "../layout/HomePageLayout";
